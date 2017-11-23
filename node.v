d7e65bea410b023ec0c650c12fddb8176450ce6f
Muyuan Cao <muyuan.cao@mail.utoronto.ca>
2017-11-23 17:28:18 -0500

    update

--- node.v
+++ node.v
@@ -8,7 +8,7 @@ module controll_node_state(clk, resetn, curr, play, reverse, set_black, next_sta
 	input reverse;
 	output reg [2:0] next_state; 
 	
-	reg [2:0] curr_state = curr, next_state;
+	reg [2:0] curr_state = curr;
 	
 	
 	localparam EMPTY = 3'b000,
@@ -28,11 +28,11 @@ module controll_node_state(clk, resetn, curr, play, reverse, set_black, next_sta
 										if (set_black)
 											next_state = BLACK;
 										else 
-											next_state = white;
+											next_state = WHITE;
 									end
 								else 
 									next_state = ENABLE;
-						  end ;
+						  end
 				BLACK : next_state = reverse ? WHITE : BLACK;
 				WHITE : next_state = reverse ? BLACK : WHITE;
 				default:next_state = EMPTY;
