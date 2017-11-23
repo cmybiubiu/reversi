7ec15bd857e40dc5f2399ad30212cc4200f4790d
Bobin <bobin.chen@mail.utoronto.ca>
2017-11-23 17:10:10 -0500

    Changed TurnManager so that the turn changes only on posedge of nextTurn.

--- TurnManager.v
+++ TurnManager.v
@@ -4,7 +4,7 @@ module TurnManager(
 				input initialize,
 				input nextTurn, //make sure nextTurn signal only goes high when all pieces that need to be changed are changed.
 				input clk,
-				input resetn
+				input resetn,
 				
 				output reg whiteOrBlack
 				);
@@ -13,13 +13,16 @@ module TurnManager(
 	
 	always@(posedge clk, posedge nextTurn)
 	begin
-		if (initialize)
+		if (initialize| resetn)
 			whiteOrBlack <= black; //start as black
-		if (resetn)
-			whiteOrBlack <= black;
-		else if (nextTurn)
+	end
+	
+	always @(posedge nextTurn)
+	begin
+		if (!initialize & !resetn)
 			whiteOrBlack <= ~whiteOrBlack;
 	end
+		
 
 endmodule 
 
