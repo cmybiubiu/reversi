19c75663e46d0ad45af5e13ce1f4bc4a729ec636
Muyuan Cao <muyuan.cao@mail.utoronto.ca>
2017-11-30 17:54:49 -0500

    place module finish

--- place.v
+++ place.v
@@ -1,31 +1,25 @@
 module place ( 
 			input clk,
-			input reseten,
+			input resetn,
 			
 			input place_enable,
 			input [191:0] curr_board,
 			input index,
 			input player_black,
 			
-			output reg [191:0] result_board;
-			output next_player_blace
+			output reg [191:0] result_board
 			);
 
 		always @(posedge clk)
 		begin
-			
-			result_board <= curr_board;
-			if (place_enable)
-				begin
-					next_player_black <= ~player_balck;
-					if (player_balck)
-						result_board[index + : 3]  <= 3'b111;
-					else 
-						result_board[index + : 3]  <= 3'b110;
-				end
-			else 
-				next_player_balck <= player_black;
-								
+					result_board <= curr_board;
+					if (place_enable)
+						begin
+							if (player_black)
+								result_board[index + : 3]  <= 3'b111;
+							else 
+								result_board[index + : 3]  <= 3'b110;
+					end								
 		end
 
 
