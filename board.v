158fd206df2114a6774e7d073f33c54c0a8465d6
Muyuan Cao <muyuan.cao@mail.utoronto.ca>
2017-11-30 17:08:51 -0500

    Change

--- board.v
+++ board.v
@@ -1,43 +1,60 @@
-// WE need a module to draw the updated  board with nodes.
+
 
 module reversi(
 			input clk,
 			input resetn,
-			input x,
-			input y,
-			input go,	
+			input [2:0]x,
+			input [2:0]y,
+			input go,
+			
+			output reg [191:0] board_result
 			);
 
-	//position 
-	integer position = y*8 + x;
-	integer index = position*3;
+	//position on the board
+	wire position = y*8 + x;
+	wire index = position*3;
+	
+	// position on the screen , still need to calculate.
+	wire [7:0]real_x;
+	wire [6:0]real_y;
+	wire [2:0]real_colour;
+	wire writen; // allow to draw.
 
 	// initial the board.   000 empty; 100 enable; 110 white; 111 black.
-	reg [191:0] board, 
-				next_board = {27{3'b000}, 3'b110, 3'b111, 6{3'b000}, 3'b111, 3'b110, 27{3'b000}};
+	reg [191:0]  board, next_board;
+	reg  check_enable, put_enable, reverse_enable, draw_enable; 
 
 	always@(posedge clk)
 		begin: 
 			if(resetn)  // initial the board.
 				board <= {27{3'b000}, 3'b110, 3'b111, 6{3'b000}, 3'b111, 3'b110, 27{3'b000}};		
 			else
-				// USE the module game_process.
-		end 
+				board <= next_board;
+		end
+		
+	game_process g0 (.clk(clk),
+					 .resetn(resetn),
+					 .go(go),
+					 .check_enable(),
+					 .put_enable(),
+					 .reverse_enable(),
+					 .draw_enable()
+					 );
+					 
+	
+		
 endmodule
 
 //Determinate the state of this game (also the board)
-module game_process (clk, resetn, curr_board, go, index, set_black, next_board);
+module game_process (clk, resetn, go, check_enable, put_enable, reverse_enable, draw_enable);
 	input clk;
-	inout resetn;
-	input [191:0] curr_board;
+	input resetn;
 	input go;
-	input index;
-	input set_black; //which player.
 
-	output reg [191:0] next_board;
+	output reg  check_enable, put_enable, reverse_enable, draw_enable;
 
 	reg [2:0] curr_state, next_state; 
-	wire [2:0] node = curr_board[index + 2: index];
+	
 
 	localparam  CHECK_ENABLE = 3'd0,
 				PUT_DWON = 3'd1,
@@ -54,20 +71,33 @@ module game_process (clk, resetn, curr_board, go, index, set_black, next_board);
 			default : next_state = FINISH;
 		endcase
 	end
-
+	
+	always@()
+	begin
+			check_enable <= 1'b0;
+			put_enable <= 1'b0;
+			reverse_enable <= 1'b0;
+			draw_enable <= 1'b0;
+		case (curr_state)
+			CHECK_ENABLE: check_enable <= 1'b1;
+			PUT_DOWN : put_enable <= 1'b1;
+			REVERSE : reverse_enable <= 1'b1;
+			FINSIH : draw_enable <= 1'b1;
+		endcase
+		
+	end
+	
+	
 	always @(*)
 	begin
-		if (curr_state == CHECK_ENABLE)
-			// Create a module to check if there are enable steps.
-		else if (curr_state == PUT_DWON)
-			// Change the state of node which the player choose. 
-			controll_node_state c1(.clk(clk), .resetn(resetn), .curr(node), .play(go), .reverse(), .set_black(set_black),  .next_state(curr_board[index + 2: index]));
-		else if (curr_state == REVERSE)
-			// Create module to reverse all possible nodes.
-		else if (curr_state == FINISH)
-			next_board <= curr_board;
+		if (!resten)
+			curr_state <= next_state;
+		else 
+			curr_state <= ENABLE;
 	end
+
 endmodule
+
 
 // Set the state of a single node.
 module controll_node_state(clk, resetn, curr, play, reverse, set_black, next_state);
@@ -79,9 +109,6 @@ module controll_node_state(clk, resetn, curr, play, reverse, set_black, next_sta
 	input reverse;
 	output reg [2:0] next_state; 
 	
-	reg [2:0] curr_state = curr, next_state;
-	
-	
 	localparam EMPTY = 3'b000,
 				  ENABLE = 3'b100,
 				  BLACK = 3'b111,
@@ -91,7 +118,7 @@ module controll_node_state(clk, resetn, curr, play, reverse, set_black, next_sta
 	// SET the next state of this node
 	always @(*)
 	begin: state_table
-			case (curr_state)
+			case (curr)
 				EMPTY: next_state = play ?  ENABLE : EMPTY;
 				ENABLE: begin 
 								if (play) 
@@ -99,13 +126,13 @@ module controll_node_state(clk, resetn, curr, play, reverse, set_black, next_sta
 										if (set_black)
 											next_state = BLACK;
 										else 
-											next_state = white;
+											next_state = WHITE;
 									end
 								else 
 									next_state = ENABLE;
-						  end ;
-				BLACK : next_state = reverse ? WHITE : BLACK;
-				WHITE : next_state = reverse ? BLACK : WHITE;
+						  end
+				BLACK : next_state = (reverse && play ) ? WHITE : BLACK;
+				WHITE : next_state = (reverse && play ) ? BLACK : WHITE;
 				default:next_state = EMPTY;
 			endcase
 	end 
