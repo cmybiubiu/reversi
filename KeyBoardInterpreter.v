ad4d519ba0013cd7cdc51ba73c6deb4e4d172694
Bobin <bobin.chen@mail.utoronto.ca>
2017-12-04 19:56:59 -0500

    Started implementing Control and changed output variable names of KeyBoardInterpreter.

--- KeyBoardInterpreter.v
+++ KeyBoardInterpreter.v
@@ -5,27 +5,27 @@ module KeyBoardInterpreter(input valid,
 									input [7:0] outCode,
 									input clk,
 									
-									output reg enterEn,
-									output reg moveRightEn,
-									output reg moveLeftEn,
-									output reg moveUpEn,
-									output reg moveDownEn
+									output reg enter,
+									output reg moveRight,
+									output reg moveLeft,
+									output reg moveUp,
+									output reg moveDown
 									);
 
 	always @(posedge valid, posedge clk)
 	begin: enable_signals
-		enterEn <= 0;
-		moveRightEn <= 0;
-		moveLeftEn <= 0;
-		moveUpEn <= 0;
-		moveDownEn <= 0;
+		enter <= 0;
+		moveRight <= 0;
+		moveLeft <= 0;
+		moveUp <= 0;
+		moveDown <= 0;
 		if (valid && makeBreak) begin
 			case(outCode)
-				8'h5A: enterEn <= 1;
-				8'h74: moveRightEn <= 1;
-				8'h6B: moveLeftEn <= 1;
-				8'h75: moveUpEn <= 1;
-				8'h72: moveDownEn <= 1;
+				8'h5A: enter <= 1;
+				8'h74: moveRight <= 1;
+				8'h6B: moveLeft <= 1;
+				8'h75: moveUp <= 1;
+				8'h72: moveDown <= 1;
 			endcase
 		end
 	end							
--- control.v
+++ control.v
@@ -4,6 +4,8 @@ module control(
 			input clk,
 			input resetn,
 			input go,
+			input validMove,
+			input hasTurn
 			
 			//add inputs from KeyBoardInterpreter here
 			
@@ -16,12 +18,49 @@ module control(
 			output reg moveDownEn
 			);
 			
-			reg [2:0] current_state, next_state; 
+			reg [8:0] current_state, next_state; 
     
-			localparam  S_LOAD_X        	= 3'd0,
-							S_LOAD_X_WAIT   	= 3'd1,
-							S_LOAD_Y_COL      = 3'd2,
-							S_LOAD_Y_COL_WAIT = 3'd3;
+		localparam  	INITIALIZE        		= 9'b000000000,
+							TO_INITIAL_RESET 			= 9'b001000001, //intermediate state
+							INITIAL_RESET   			= 9'b000000001,
+							INITIAL_DRAW_BOARD   	= 9'b000000011,
+							INITIAL_DRAW_PIECES 		= 9'b000001011,
+							WAIT_FOR_INPUT				= 9'b000011011,
+							TO_MOVE_RIGHT 				= 9'b000111011, //intermediate state
+							MOVE_RIGHT 					= 9'b000110011,
+							TO_MOVE_LEFT 				= 9'b001011011, //intermediate state
+							MOVE_LEFT 					= 9'b001010011, 
+							TO_MOVE_UP 					= 9'b010011011, //intermediate state
+							MOVE_UP 						= 9'b010010011, 
+							TO_MOVE_DOWN 				= 9'b100011011, //intermediate state
+							MOVE_DOWN 					= 9'b100010011,
+							DRAW_MOVE 					= 9'b000010011,
+							CHECK_IF_VALID_MOVE 		= 9'b000111011,
+							AFTER_CHECK_IF_VALID		= 9'b000101011,
+							TO_DISPLAY_INVALID_1 	= 9'b000101010, //intermediate state
+							TO_DISPLAY_INVALID_2 	= 9'b000100010, //intermediate state
+							DISPLAY_INVALID 			= 9'b000110010,
+							DISPLAY_INVALID_WAIT 	= 9'b000111010,
+							ERASE_INVALID 				= 9'b000011010,
+							PLACE_AND_DRAW_PIECE 	= 9'b001101011,
+							FLIP_AND_DRAW_PIECES 	= 9'b011101011,
+							UPDATE_SCORE				= 9'b011100011,
+							DRAW_SCORE 					= 9'b011100111,
+							DETERMINE_OPPONENT_HAS_TURN = 9'b010100111,
+							TO_OPPONENT_HAS_TURN 	= 9'b0010110111, //intermediate state
+							OPPONENT_HAS_TURN 		= 9'b000110111,
+							DETERMINE_CURRENT_HAS_TURN = 9'b000010111,
+							CURRENT_HAS_TURN 			= 9'b000011111,
+							TO_SWITCH_TURN_1			= 9'b000111111, //intermediate state
+							TO_SWITCH_TURN_2 			= 9'b000111101, //intermediate state
+							TO_SWITCH_TURN_3 			= 9'b000111001, //intermediate state
+							SWITCH_TURN 				= 9'b000011001, 
+							END_GAME 					= 9'b001011111,
+							DISPLAY_WINNER 			= 9'b001001111,
+							DISPLAY_WINNER_WAIT 		= 9'b001000111,
+							CLEAR_PIECES 				= 9'b001000011;
+							
+							
 							
 			// Next state logic aka our state table
 			always @(*)
@@ -31,7 +70,50 @@ module control(
 						S_LOAD_X_WAIT: next_state = gox ? S_LOAD_X_WAIT : S_LOAD_Y_COL;
 						S_LOAD_Y_COL: next_state = go ? S_LOAD_Y_COL_WAIT : S_LOAD_Y_COL;
 						S_LOAD_Y_COL_WAIT: next_state = go ? S_LOAD_Y_COL_WAIT : S_LOAD_X;
-						default: next_state = S_LOAD_X;
+						
+						INITIALIZE:        		next_state = go ? INITIAL_RESET : INITIALIZE,
+						TO_INITIAL_RESET: 		next_state = 9'b001000001, //intermediate state
+						INITIAL_RESET:   			next_state = 9'b000000001,
+						INITIAL_DRAW_BOARD:   	next_state = 9'b000000011,
+						INITIAL_DRAW_PIECES: 	next_state = 9'b000001011,
+						WAIT_FOR_INPUT: begin 	
+														next_state = 9'b000011011;
+														next_state = ;
+											 end 
+						TO_MOVE_RIGHT: 			next_state = 9'b000111011, //intermediate state
+						MOVE_RIGHT: 				next_state = 9'b000110011,
+						TO_MOVE_LEFT: 				next_state = 9'b001011011, //intermediate state
+						MOVE_LEFT: 					next_state = 9'b001010011, 
+						TO_MOVE_UP: 				next_state = 9'b010011011, //intermediate state
+						MOVE_UP: 					next_state = 9'b010010011, 
+						TO_MOVE_DOWN: 				next_state= 9'b100011011, //intermediate state
+						MOVE_DOWN: 					next_state= 9'b100010011,
+						DRAW_MOVE: 					next_state= 9'b000010011,
+						CHECK_IF_VALID_MOVE: 	next_state	= 9'b000111011,
+						AFTER_CHECK_IF_VALID:	next_state	= 9'b000101011,
+						TO_DISPLAY_INVALID_1: 	next_state= 9'b000101010, //intermediate state
+						TO_DISPLAY_INVALID_2: 	next_state= 9'b000100010, //intermediate state
+						DISPLAY_INVALID: 			next_state= 9'b000110010,
+						DISPLAY_INVALID_WAIT: 	next_state= 9'b000111010,
+						ERASE_INVALID: 			next_state	= 9'b000011010,
+						PLACE_AND_DRAW_PIECE: 	next_state= 9'b001101011,
+						FLIP_AND_DRAW_PIECES: 	next_state= 9'b011101011,
+						UPDATE_SCORE:				next_state= 9'b011100011,
+						DRAW_SCORE: 				next_state	= 9'b011100111,
+						DETERMINE_OPPONENT_HAS_TURN: next_state= 9'b010100111,
+						TO_OPPONENT_HAS_TURN: 	next_state= 9'b0010110111, //intermediate state
+						OPPONENT_HAS_TURN: 		next_state= 9'b000110111,
+						DETERMINE_CURRENT_HAS_TURN: next_state= 9'b000010111,
+						CURRENT_HAS_TURN: 		next_state	= 9'b000011111,
+						TO_SWITCH_TURN_1:			next_state= 9'b000111111, //intermediate state
+						TO_SWITCH_TURN_2: 		next_state	= 9'b000111101, //intermediate state
+						TO_SWITCH_TURN_3: 		next_state	= 9'b000111001, //intermediate state
+						SWITCH_TURN: 				next_state= 9'b000011001, 
+						END_GAME: 					next_state= 9'b001011111,
+						DISPLAY_WINNER: 			next_state= 9'b001001111,
+						DISPLAY_WINNER_WAIT: 	next_state	= 9'b001000111,
+						CLEAR_PIECES: 				next_state= 9'b001000011;
+						default: next_state = INITIALIZE;
 					endcase
 			end
 			
