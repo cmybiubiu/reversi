adf2fcbdc4f81907798c74922c217f3e42eb26bf
Muyuan Cao <muyuan.cao@mail.utoronto.ca>
2017-12-05 19:44:06 -0500

    Change

--- checkIfValidMove.v
+++ checkIfValidMove.v
@@ -9,8 +9,31 @@ module checkIfValidMove (
             input [127:0] board,
 
             output [7:0] valids,
-            output [47:0] end_points
+            output [47:0] end_points,
+				output checkIfValidMoveDone
 );
+
+	 
+	 reg [7:0] checks = 0;
+	 
+	 always @(posedge clk)
+	 begin
+		if (resetn) 
+			begin 			
+				valids <= 0;
+				end_points <= 0;
+				checkIfValidMoveDone <= 0;
+			end 
+		else
+			begin
+				if (checks == 8'b1111_1111)
+					checkIfValidMoveDone <= 1;
+				else
+					checkIfValidMoveDone <= 0;
+			end
+
+			
+	 end
     checkValidHelper ch0(
         .clk(clk),
         .resetn(resetn),
@@ -21,7 +44,8 @@ module checkIfValidMove (
         .player_black(player_black),
 
         .valid(valids[0]),
-        .end_point(end_points[5:0])
+        .end_point(end_points[5:0]),
+		  .checkDone(checks[0])
         );
 
     checkValidHelper ch1(
@@ -34,7 +58,8 @@ module checkIfValidMove (
         .player_black(player_black),
 
         .valid(valids[1]),
-        .end_point(end_points[11:6])
+        .end_point(end_points[11:6]),
+		  .checkDone(checks[1])
         );
     
     checkValidHelper ch2(
@@ -47,7 +72,8 @@ module checkIfValidMove (
         .player_black(player_black),
 
         .valid(valids[2]),
-        .end_point(end_points[17:12])
+        .end_point(end_points[17:12]),
+		  .checkDone(checks[2])
         );
 
     checkValidHelper ch3(
@@ -60,7 +86,8 @@ module checkIfValidMove (
         .player_black(player_black),
 
         .valid(valids[3]),
-        .end_point(end_points[23:18])
+        .end_point(end_points[23:18]),
+		  .checkDone(checks[3])
         );
     
     checkValidHelper ch4(
@@ -73,7 +100,8 @@ module checkIfValidMove (
         .player_black(player_black),
 
         .valid(valids[4]),
-        .end_point(end_points[29:24])
+        .end_point(end_points[29:24]),
+		  .checkDone(checks[4])
         );
     
     checkValidHelper ch5(
@@ -86,7 +114,8 @@ module checkIfValidMove (
         .player_black(player_black),
 
         .valid(valids[5]),
-        .end_point(end_points[35:30])
+        .end_point(end_points[35:30]),
+		  .checkDone(checks[5])
         );
     
     checkValidHelper ch6(
@@ -99,7 +128,8 @@ module checkIfValidMove (
         .player_black(player_black),
 
         .valid(valids[6]),
-        .end_point(end_points[41:36])
+        .end_point(end_points[41:36]),
+		  .checkDone(checks[6])
         );
 
     checkValidHelper ch7(
@@ -112,6 +142,7 @@ module checkIfValidMove (
         .player_black(player_black),
 
         .valid(valids[7]),
-        .end_point(end_points[47:42])
+        .end_point(end_points[47:42]),
+		  .checkDone(checks[7])
         );
 endmodule
